01536e39c96ca1fb87cd336624fd616c7cc39b55
Chaitanya <iamcvers@gmail.com>
2022-11-21 00:38:36 +0530

    initial commit with basic server, user model and user auth

---
+++ server/controllers/auth.js
@@ -0,0 +1,51 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+//register
+export const register = async (req, res, next) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = await User.create({
+      ...req.body,
+      password: passwordHash,
+      viewedProfile: 0,
+      impressions: 0,
+    });
+
+    const newSavedUser = await newUser.save();
+    delete newSavedUser._doc.password
+    res.status(201).json({ data: newSavedUser });
+  } catch (e) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+
+//logging
+
+export const login  = async(req,res) =>{
+    try {
+        const {email,password} = req.body;
+        const user = await User.findOne({
+            email:email
+        })
+
+        if (!user) return res.status(400).json({message:'user not found'})
+        
+        const isMatch = bcrypt.compare(password,user.password)
+
+        if (!isMatch) return res.status(400).json({message:"invalid credentials"})
+
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+        
+        delete user._doc.password;
+        
+        res.status(200).json({token,user})
+
+    } catch(e){
+        res.status(500).json({err:e.message})
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,70 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import {register} from './controllers/auth.js'
+import authRoutes from './routes/auth.js'
+// Configuration
+const PORT = process.env.PORT || 3001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+
+// fileStore
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage }); //to upload user files to disk
+
+//auth routes
+
+app.post('/auth/register',upload.single('picture'),register)
+
+
+app.use('/auth',authRoutes)
+
+
+
+
+
+
+//mongoose
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server PORT: ${PORT}(running...)`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Database connection failed. PORT${PORT}`);
+    console.log(err);
+  });
